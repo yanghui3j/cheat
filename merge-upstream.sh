725d974ab658cee6b18684c94686f6761f15384f
James Zhu <zhujian0805@gmail.com>
2015-03-27 11:53:36 +0800

    mod

---
+++ merge-upstream.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE: merge-upstream.sh
+# 
+#         USAGE: ./merge-upstream.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: James Zhu (000), zhujian0805@gmail.com
+#  ORGANIZATION: JZ
+#       CREATED: 2015年03月27日 11时52分35秒 CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+git checkout master
+git pull git@github.com:chrisallenlane/cheat.git
+git push origin master
